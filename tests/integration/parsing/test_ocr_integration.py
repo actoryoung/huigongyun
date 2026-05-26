0d00ad10cd4d836dd187b4472ff315a46eca6eaa
actoryoung <2776137609@qq.com>
2026-05-26 12:58:34 +0800

    test(ocr): add integration test placeholder (skipped by default)

---
+++ tests/integration/parsing/test_ocr_integration.py
@@ -0,0 +1,12 @@
+import os
+import pytest
+
+
+@pytest.mark.skipif(os.environ.get('OCR_POC') != '1', reason='OCR integration tests are skipped by default')
+def test_ocr_integration(ocr_sample_path):
+    from huigongyun.parsing.ocr_adapter import TesseractAdapter
+
+    # This will run a real OCR pass; ensure system tesseract and poppler are installed.
+    out = TesseractAdapter.image_to_dict(str(ocr_sample_path))
+    assert 'pages' in out
+    assert isinstance(out['pages'], list)
